4b3a1b360cefa1c27a71aff3abbaed337d9a458c
David Jennes <david.jennes@gmail.com>
2020-10-11 17:32:07 +0200

    Adapt tests to use new testutils package

--- Tests/SwiftGenKitTests/ColorsXMLFileTests.swift
+++ Tests/SwiftGenKitTests/ColorsXMLFileTests.swift
@@ -6,6 +6,7 @@
 
 import PathKit
 @testable import SwiftGenKit
+import TestUtils
 import XCTest
 
 final class ColorsXMLFileTests: XCTestCase {
@@ -13,7 +14,7 @@ final class ColorsXMLFileTests: XCTestCase {
     let parser = try Colors.Parser()
     let options = try ParserOptionValues(options: [:], available: Colors.XMLFileParser.allOptions)
     let xmlParser = Colors.XMLFileParser(options: options)
-    parser.palettes = [try xmlParser.parseFile(at: Fixtures.path(for: "colors.xml", sub: .colors))]
+    parser.palettes = [try xmlParser.parseFile(at: Fixtures.resource(for: "colors.xml", sub: .colors))]
 
     let result = parser.stencilContext()
     XCTDiffContexts(result, expected: "defaults", sub: .colors)
@@ -23,7 +24,7 @@ final class ColorsXMLFileTests: XCTestCase {
     let parser = try Colors.Parser()
     let options = try ParserOptionValues(options: ["colorFormat": "argb"], available: Colors.XMLFileParser.allOptions)
     let xmlParser = Colors.XMLFileParser(options: options)
-    parser.palettes = [try xmlParser.parseFile(at: Fixtures.path(for: "colors-argb.xml", sub: .colors))]
+    parser.palettes = [try xmlParser.parseFile(at: Fixtures.resource(for: "colors-argb.xml", sub: .colors))]
 
     let result = parser.stencilContext()
     XCTDiffContexts(result, expected: "argb", sub: .colors)
@@ -33,7 +34,7 @@ final class ColorsXMLFileTests: XCTestCase {
     do {
       let options = try ParserOptionValues(options: ["colorFormat": "bad"], available: Colors.XMLFileParser.allOptions)
       let xmlParser = Colors.XMLFileParser(options: options)
-      _ = try xmlParser.parseFile(at: Fixtures.path(for: "colors-argb.xml", sub: .colors))
+      _ = try xmlParser.parseFile(at: Fixtures.resource(for: "colors-argb.xml", sub: .colors))
       XCTFail("Code did parse file successfully while it was expected to fail for unsupported color format")
     } catch Colors.ParserError.unsupportedColorFormat {
       // That's the expected exception we want to happen
@@ -46,7 +47,7 @@ final class ColorsXMLFileTests: XCTestCase {
     do {
       let options = try ParserOptionValues(options: [:], available: Colors.XMLFileParser.allOptions)
       let xmlParser = Colors.XMLFileParser(options: options)
-      _ = try xmlParser.parseFile(at: Fixtures.path(for: "bad-syntax.xml", sub: .colors))
+      _ = try xmlParser.parseFile(at: Fixtures.resource(for: "bad-syntax.xml", sub: .colors))
       XCTFail("Code did parse file successfully while it was expected to fail for bad syntax")
     } catch Colors.ParserError.invalidFile {
       // That's the expected exception we want to happen
@@ -59,7 +60,7 @@ final class ColorsXMLFileTests: XCTestCase {
     do {
       let options = try ParserOptionValues(options: [:], available: Colors.XMLFileParser.allOptions)
       let xmlParser = Colors.XMLFileParser(options: options)
-      _ = try xmlParser.parseFile(at: Fixtures.path(for: "bad-value.xml", sub: .colors))
+      _ = try xmlParser.parseFile(at: Fixtures.resource(for: "bad-value.xml", sub: .colors))
       XCTFail("Code did parse file successfully while it was expected to fail for bad value")
     } catch Colors.ParserError.invalidHexColor(path: _, string: "this isn't a color", key: "ArticleTitle") {
       // That's the expected exception we want to happen
